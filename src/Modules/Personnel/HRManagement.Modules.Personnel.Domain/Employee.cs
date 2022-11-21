ad2ff7db60b397bc2ca79a86fb25feded5b68705
Gabin Dongmo <gdongmo@gmail.com>
2022-11-20 23:36:23 -0500

    Setting up Personnel management domain

---
+++ src/Modules/Personnel/HRManagement.Modules.Personnel.Domain/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace HRManagement.Modules.Personnel.Domain;
+
+public class Employee
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateOnly DateOfBirth { get; set; }
+    public DateOnly HireDate { get; set; }
+    public DateOnly? TerminationDate { get; set; }
+}
