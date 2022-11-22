f557c994cb2744909c40bfeef8e3bc91eea93370
Gabin Dongmo <gdongmo@gmail.com>
2022-11-21 23:08:27 -0500

    Improving domain model

--- src/Modules/Personnel/HRManagement.Modules.Personnel.Domain/Employee/Employee.cs
+++ src/Modules/Personnel/HRManagement.Modules.Personnel.Domain/Employee/Employee.cs
@@ -1,4 +1,6 @@
-﻿namespace HRManagement.Modules.Personnel.Domain.Employee;
+﻿using CSharpFunctionalExtensions;
+
+namespace HRManagement.Modules.Personnel.Domain.Employee;
 
 public class Employee : Entity<Guid>
 {
@@ -6,13 +8,22 @@ public class Employee : Entity<Guid>
     public EmailAddress EmailAddress { get; }
     public DateOfBirth DateOfBirth { get; }
     public DateOnly HireDate { get; }
-    public DateOnly? TerminationDate { get; set; }
+    public DateOnly? TerminationDate { get; }
 
-    public Employee(Name name, EmailAddress emailAddress, DateOfBirth dateOfBirth)
+    private Employee(Name name, EmailAddress emailAddress, DateOfBirth dateOfBirth)
     {
         Name = name;
         EmailAddress = emailAddress;
         DateOfBirth = dateOfBirth;
         HireDate = DateOnly.FromDateTime(DateTime.Now);
+    }
+
+    public static Result<Employee, Error> Create(Name name, EmailAddress emailAddress, DateOfBirth dateOfBirth)
+    {
+        if (name == null) throw new ArgumentNullException(nameof(name));
+        if (emailAddress == null) throw new ArgumentNullException(nameof(emailAddress));
+        if (dateOfBirth == null) throw new ArgumentNullException(nameof(dateOfBirth));
+
+        return new Employee(name, emailAddress, dateOfBirth);
     }
 }
