3f9ad78f158ca411fc6697d644c2c204ccce6b14
Gabin Dongmo <gdongmo@gmail.com>
2023-01-05 11:30:47 -0500

    setting up minimal api

---
+++ src/HRManagement.Api/Endpoints/EmployeesManagement.cs
@@ -0,0 +1,63 @@
+﻿using AutoMapper;
+using CSharpFunctionalExtensions;
+using HRManagement.Api.Models;
+using HRManagement.Modules.Personnel.Application.DTOs;
+using HRManagement.Modules.Personnel.Application.Features.Employee;
+using MediatR;
+
+namespace HRManagement.Api.Endpoints;
+
+public static class EmployeesManagement
+{
+    public static void AddEmployeesManagementEndpoints(this IEndpointRouteBuilder app)
+    {
+        const string employees = "/employees";
+
+        app.MapGet(employees, async (IMediator mediator) =>
+        {
+            var request = new GetEmployeesQuery();
+            var (isSuccess, _, value, error) = await mediator.Send(request);
+            return isSuccess
+                ? Results.Ok(ApiResponse<List<EmployeeDto>>.Ok(value))
+                : Results.BadRequest(ApiResponse<List<EmployeeDto>>.Error(error));
+        });
+
+        app.MapGet($"{employees}/{{id}}", async (IMediator mediator, string id) =>
+        {
+            var request = new GetEmployeeQuery {EmployeeId = id};
+            var (isSuccess, _, value, error) = await mediator.Send(request);
+            return isSuccess
+                ? Results.Ok(ApiResponse<EmployeeDto>.Ok(value))
+                : Results.BadRequest(ApiResponse<EmployeeDto>.Error(error));
+        });
+
+        app.MapPost(employees, async (IMediator mediator, IMapper mapper, HireEmployeeDto newEmployee) =>
+        {
+            var (isSuccess, _, employee, error) = await mediator.Send(mapper.Map<HireEmployeeCommand>(newEmployee));
+            return isSuccess
+                ? Results.Created($"{employees}/{{id}}", ApiResponse<EmployeeDto>.Ok(employee))
+                : Results.BadRequest(ApiResponse<EmployeeDto>.Error(error));
+        });
+
+        app.MapPut($"{employees}/{{id}}", async (IMediator mediator, IMapper mapper, string id, UpdateEmployeeDto updatedEmployee) =>
+            {
+                var command = mapper.Map<UpdateEmployeeCommand>(updatedEmployee);
+                command.EmployeeId = id;
+                var (isSuccess, _, _, error) = await mediator.Send(command);
+                return isSuccess ? Results.NoContent() : Results.BadRequest(ApiResponse<Unit>.Error(error));
+            });
+
+        app.MapPut($"{employees}/{{id}}/terminate", async (IMediator mediator, string id) =>
+        {
+            var (isSuccess, _, _, error) = await mediator.Send(new TerminateEmployeeCommand {EmployeeId = id});
+            return isSuccess ? Results.NoContent() : Results.BadRequest(ApiResponse<Unit>.Error(error));
+        });
+
+        app.MapDelete($"{employees}/{{id}}", async (IMediator mediator, string id) =>
+        {
+            var command = new HardDeleteEmployeeCommand {EmployeeId = id};
+            var (isSuccess, _, _, error) = await mediator.Send(command);
+            return isSuccess ? Results.NoContent() : Results.BadRequest(ApiResponse<Unit>.Error(error));
+        });
+    }
+}
--- src/HRManagement.Api/Program.cs
+++ src/HRManagement.Api/Program.cs
@@ -1,4 +1,4 @@
-using HRManagement.Common.Domain;
+using HRManagement.Api.Endpoints;
 using HRManagement.Common.Domain.Models;
 using HRManagement.Modules.Personnel.Application;
 using HRManagement.Modules.Personnel.Persistence;
@@ -14,7 +14,6 @@ builder.Services.AddAutoMapper(typeof(Program));
 builder.Services.AddMediatR(typeof(Program));
 builder.Services.AddApplicationServices();
 builder.Services.AddPersistenceServices();
-builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.Configure<RouteOptions>(options => options.LowercaseUrls = true);
@@ -31,13 +30,8 @@ if (app.Environment.IsDevelopment())
         var services = serviceScope.ServiceProvider;
         var personnelDbContext = services.GetRequiredService<PersonnelDbContext>();
         DataSeeder.SeedEmployees(personnelDbContext);
-    }
-}
+    }}
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
+app.AddEmployeesManagementEndpoints();
 
 app.Run();
